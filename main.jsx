9fdc8072a893abcdf4f7407419edf693b18f0ee9
Russell Eshiet <russelleshiet107@gmail.com>
2026-06-07 20:51:22 +0100

    changed directory of main.jsx

--- main.jsx
+++ main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App";
+import App from "./src/App";
 
 import "./styles/global.css";
 import "./styles/admin.css";
 import "./styles/responsive.css";
 
-import CartProvider from "./context/CartContext";
+import CartProvider from "./src/context/CartContext";
 
-import AuthProvider from "./context/AuthContext";
+import AuthProvider from "./src/context/AuthContext";
 
 ReactDOM.createRoot(
   document.getElementById("root")
