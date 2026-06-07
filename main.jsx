4383e88b024a7e6c01a5ba0b28b9117941066029
Russell Eshiet <russelleshiet107@gmail.com>
2026-06-07 21:10:15 +0100

    updated import path for the second time

--- main.jsx
+++ main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 
 import App from "./src/App";
 
-import "./styles/global.css";
-import "./styles/admin.css";
-import "./styles/responsive.css";
+import "./src/styles/global.css";
+import "./src/styles/admin.css";
+import "./src/styles/responsive.css";
 
 import CartProvider from "./src/context/CartContext";
 
